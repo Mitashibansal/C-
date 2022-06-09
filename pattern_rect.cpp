0e9604dbc03e762228d5b5198930590f6d6b340a
Mitashibansal <84284893+Mitashibansal@users.noreply.github.com>
2022-06-09 22:10:35 +0530

    Create pattern_rect.cpp

---
+++ pattern_rect.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int c,r;
+    cin>>r>>c;
+    for(int i=1;i<=r;i++)
+    {
+        for(int i=1;i<=c;i++)
+        cout<<"$ ";
+    cout<<"\n";
+    }
+}
