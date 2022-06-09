c8f2dcaf17f12bff5ace25d1d10ef678b9b92c4a
Mitashibansal <84284893+Mitashibansal@users.noreply.github.com>
2022-06-09 21:59:01 +0530

    Create pythagorean_triplet.cpp

---
+++ pythagorean_triplet.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool check(int a,int b,int c)
+{
+    int x =max(a,max(b,c));
+    int y,z;
+    if(x==a){
+        y=b;
+        z=c;
+    }
+    else if(x==b){
+        y=a;
+        z=c;
+    }
+    else{
+        y=a;
+        z=b;
+    }
+    if (x*x == y*y + z*z)
+        return true;
+    return false;
+}
+int32_t main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+if (check(a,b,c))
+cout<<"YES";
+else
+cout<<"NO";
+}
